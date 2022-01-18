5265f25d4e2cce79a0f129b6df1c39ca1446c8ee
chinmay mohanty <chinmaymohanty21@gmail.com>
2022-01-18 22:55:20 +0530

    Efficiency Of Engine using virtual function

---
+++ C++/EfficiencyOfEngine.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include<conio.h>
+using namespace std;
+class Engine			//abstract base class 
+{
+protected:
+  float input, output;
+public:
+  void getInput ()
+  {
+    cin >> input;
+  }
+  void getOutput ()
+  {
+    cin >> output;
+  }
+  virtual float GetEfficiency () = 0;	//pure virtual function
+};
+
+class InternalCombustineEngine:public Engine
+{
+public:
+  float GetEfficiency ()
+  {
+
+    return output / input;	//calculation of efficiency of engine 
+  }
+};
+
+class ExternalCombustineEngine:public Engine
+{
+public:
+  float GetEfficiency ()
+  {
+
+    return output / input;
+  }
+};
+
+class PetrolEngine:public Engine
+{
+public:
+  float GetEfficiency ()
+  {
+
+    return output / input;
+  }
+};
+
+
+class DieselEngine : public Engine
+  {
+    public:
+          float GetEfficiency(){
+
+                    return output/input; 
+          }
+  }; 
+  
+  class SteamEngine : public Engine
+  {
+    public:
+          float GetEfficiency(){
+
+                    return output/input; 
+          }
+  }; 
+
+int main ()
+{
+
+  int num;
+  cout << "Enter 1 for Internal Combustine Engine" << endl;
+  cout << "Enter 2 for External Combustine Engine" << endl;
+  cout << "Enter 3 for Petrol Engine" << endl;
+  cout << "Enter 4 for Diesel Engine" << endl;
+  cout << "Enter 5 for Steam Engine" << endl;
+  cin >> num;
+  switch (num)
+    {
+    case 1:
+      {
+	InternalCombustineEngine in;
+	cout << "Enter the input for Internal Combustine Engine: " << endl;
+	in.getInput();
+	cout << "Enter the output for Internal Combustine Engine: " << endl;
+	in.getOutput();
+	cout << "Efficiency of Internal Combustine Engine is: " << 
+	in.GetEfficiency() <<endl;
+      }
+      break;
+    case 2:
+      {
+	ExternalCombustineEngine ex;
+	cout << "Enter the input for External Combustine Engine: " << endl;
+	ex.getInput ();
+	cout << "Enter the output for External Combustine Engine: " << endl;
+	ex.getOutput();
+	cout << "Efficiency of External Combustine Engine is: " << 
+	ex.GetEfficiency() << endl;
+      }
+      break;
+    case 3:
+      {
+	PetrolEngine pt;
+	cout << "Enter the input for Petrol Engine: " << endl;
+	pt.getInput();
+	cout << "Enter the output for Petrol Engine: " << endl;
+	pt.getOutput();
+	cout << "Efficiency of Petrol Engine is: " << 
+	pt.GetEfficiency() <<endl;
+      }
+      break;
+      case 4:
+      {
+	DieselEngine di;
+	cout << "Enter the input for Diesel Engine: " << endl;
+	di.getInput ();
+	cout << "Enter the output for Diesel Engine: " << endl;
+	di.getOutput ();
+	cout << "Efficiency of Petrol Engine is: " << 
+	di.GetEfficiency () <<endl;
+      }
+      break;
+      case 5:
+      {
+	SteamEngine st;
+	cout << "Enter the input for Steam Engine: " << endl;
+	st.getInput ();
+	cout << "Enter the output for Steam Engine: " << endl;
+	st.getOutput ();
+	cout << "Efficiency of Steam Engine is: " << 
+	st.GetEfficiency () <<endl;
+      }
+      break;
+    default:
+      cout << "please choose between 1 to 5" << endl;
+      break;
+    }
+}
