b0b4160a22a5e90eed80f58a68e09b236d4111ff
chinmay mohanty <chinmaymohanty21@gmail.com>
2022-03-01 13:33:27 +0530

    Xamarine form app it calculate simple intrest

---
+++ SimpleInterestApp/SimpleInterestApp/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace SimpleInterestApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected void Button_Clicked(object sender, EventArgs e)
+        {
+            double total = Convert.ToDouble(principleAmount.Text)  * Convert.ToDouble(months.Text) * Convert.ToDouble(intrest.Text) / 100;
+            Atotal.Text = total.ToString();
+
+        }
+    }
+}
