cfe3cb432261c9d17bb86f024e5ea3a363847301
chinmay mohanty <chinmaymohanty21@gmail.com>
2022-03-09 08:19:57 +0530

    python Automation

--- python projects/pythonAutomationOfSalesMate.py
+++ python projects/pythonAutomationOfSalesMate.py
@@ -8,6 +8,8 @@ print("SalesMate + Backup Manu Test")
 
 
 #get the width and height of the monitor
+#screen size is normal 1920x1080
+
 width= GetSystemMetrics(0)
 height=GetSystemMetrics(1)
 #click on the botton left to launch the start menu
@@ -21,34 +23,37 @@ pyautogui.press("enter",1,10)
 #Sometimes SalesMate + might take 10 seconds to load it . So 10 sec delay
 print("Press Enter Key again to close the Tip Of the Day Dialog")
 pyautogui.press("enter")
-pyautogui.sleep(2)  #wait for two seconds
+pyautogui.sleep(3)  #wait for two seconds
 print("Now press Esc for full screen mode  ")
 pyautogui.press("Esc")
+
+pyautogui.sleep(3)
 print("open for printing bar code ")
 pyautogui.hotkey('Ctrl','b')
 pyautogui.sleep(2)
-
+#add the product name
 pyautogui.leftClick(x=1286, y=364)
 pyautogui.typewrite("skin care")
 
 pyautogui.sleep(2)
-
+#add the product company name 
 pyautogui.leftClick(x=1316, y=406)
 pyautogui.typewrite("Biotique")
 
 pyautogui.sleep(2)
-
+#add the product price
 pyautogui.leftClick(x=1303, y=461)
 pyautogui.typewrite("200")
 pyautogui.leftClick(x=1384, y=810)
 
 pyautogui.sleep(2)
-
+#save the barcode of the product 
 pyautogui.typewrite("Biotique product")
 pyautogui.sleep(2)
 
 pyautogui.press('enter')
 pyautogui.sleep(2)
+#close salesmate+ application
 pyautogui.leftClick(x=1484, y=900)
 pyautogui.sleep(2)
 pyautogui.leftClick(x=18, y=34)
